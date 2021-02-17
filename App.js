0d2298e2026690065c5c57801a6a003575b566b7
butterfly2080 <butterfly2080@outlook.com>
2021-02-18 00:23:57 +0800

    handle ratio, volume for android

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -16,14 +16,32 @@ import {
   StatusBar,
   TouchableOpacity,
   Platform,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  DeviceEventEmitter
 } from 'react-native';
 import axios from 'axios';
 import Orientation from 'react-native-orientation-locker';
 import {RNCamera} from 'react-native-camera';
 import SystemSetting from 'react-native-system-setting';
 
+
 const App: () => React$Node = () => {
   const [loading, setLoading] = useState(false);
+  const [ratio, setRatio] = useState('4:3')
+  const prepareRatio = async (DESIRED_RATIO) => {
+    if (Platform.OS === 'android' && cameraRef.current) {
+         const ratios = await cameraRef.current.getSupportedRatiosAsync();
+         console.log('=========ratios==========') 
+         console.log(ratios)
+         // See if the current device has your desired ratio, otherwise get the maximum supported one
+         // Usually the last element of "ratios" is the maximum supported ratio
+         const ratio = ratios.find((ratio) => ratio === DESIRED_RATIO) || ratios[ratios.length - 1];
+         
+         setRatio(ratio);
+    }
+}
+
   useEffect(() => {
     Orientation.lockToLandscape();
     const volumeListener = SystemSetting.addVolumeListener((data) => {
@@ -31,10 +49,15 @@ const App: () => React$Node = () => {
       console.log('===vol===', volume);
       handleShoot();
     });
+    const listener = DeviceEventEmitter.addListener('volume-click', handleVolume)
     return () => {
-      SystemSetting.removeListener(volumeListener);
+      DeviceEventEmitter.removeListener(listener);
     };
   }, []);
+  const handleVolume = (data) =>{
+    console.log('===vol===', data);
+    handleShoot();
+  }
   const cameraRef = useRef(null);
   const handleShoot = async () => {
     if (cameraRef.current && !loading) {
@@ -53,6 +76,13 @@ const App: () => React$Node = () => {
       uploadImage(data);
     }
   };
+  const handleRatio = () =>{
+    console.log("=============hey handle ratio============")
+    if(ratio === '4:3')
+      prepareRatio('1:1')
+    else
+      prepareRatio('4:3')  
+  }
   const uploadImage = (data) => {
     let base_url = 'https://hey.com/post';
     setLoading(true);
@@ -71,18 +101,35 @@ const App: () => React$Node = () => {
         setLoading(false);
       });
   };
- 
+  const boxStyle = useCallback(
+    () => {
+      if(ratio==='4:3')
+        return {
+          width: 400,
+          height: 300,
+          backgroundColor: 'blue',
+          alignSelf: 'center',
+          borderColor:'red',
+          borderWidth: 1
+        }
+      else
+        return {
+          width: 400,
+          height: 400,
+          backgroundColor: 'blue',
+          alignSelf: 'center',
+          borderColor:'red',
+          borderWidth: 1
+        }
+    },
+    [ratio],
+  )
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={{backgroundColor: 'grey', flexGrow: 1}}>
         <View
-          style={{
-            width: '50%',
-            height: '50%',
-            backgroundColor: 'blue',
-            alignSelf: 'center',
-          }}>
+          style={boxStyle()}>
           <RNCamera
             ref={cameraRef}
             style={styles.preview}
@@ -94,11 +141,20 @@ const App: () => React$Node = () => {
               buttonPositive: 'Ok',
               buttonNegative: 'Cancel',
             }}
+            ratio={ratio}
             captureAudio={false}
           />
+         
         </View>
+        <View>
+          <TouchableOpacity style={{alignSelf:'flex-end'}} onPress={()=>handleRatio()}>
+            <View style={styles.switchBtn} >
+            </View>
+          </TouchableOpacity>
+          </View>
         <View style={{flexGrow: 1}}></View>
         <View>
+          
           <TouchableOpacity
             style={{
               width: 100,
@@ -120,7 +176,13 @@ const styles = StyleSheet.create({
   preview: {
     width: '100%',
     height: '100%',
+    borderColor: 'white',
+    borderWidth: 2
   },
+  switchBtn:{
+    zIndex:999,
+    width: 100, height: 100, backgroundColor: 'green',
+  }
 });
 
 export default App;
